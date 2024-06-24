0dcb8f60f348d469838aad809d07be6a15f6ca3c
Matthieu SERRANO <matthieu.serrano@outlook.com>
2024-06-24 18:43:56 +0200

    valid wp

--- main.tf
+++ main.tf
@@ -50,20 +50,3 @@ resource "aws_security_group_rule" "allow_mysql_access_from_wordpress" {
 
   depends_on = [module.autoscaling]
 }
-
-# resource "aws_security_group_rule" "apply_public_access_to_rds" {
-#   type              = "ingress"
-#   from_port         = 3306
-#   to_port           = 3306
-#   protocol          = "tcp"
-#   security_group_id = module.rds.rds_sg_id
-#   source_security_group_id = module.autoscaling.autoscaling_security_group_id
-#   # cidr_blocks       = ["0.0.0.0/0"]
-#   # self              = true
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-#   depends_on = [module.autoscaling]
-# }
---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.31"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "eu-west-3"
+#   shared_credentials_files = ["~/.aws/credentials"]
+}
+
+# provider "http" {
+# }
